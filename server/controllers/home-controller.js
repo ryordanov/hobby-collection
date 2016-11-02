8b92f9c73e742b5dc02669cc19d8d7a08352cf0d
Rosen Yordanov <workbox@abv.bg>
2016-11-02 22:46:08 +0200

    css beautify

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -1,6 +1,11 @@
+let series = [
+    'Turbo 1-50',
+    'Turbo 51-120',
+    'Bi-bib'
+];
 module.exports = {
     index : (req, res) => {
-        res.render('home/index');
+        res.render('home/index', { title: series[0], series: series });
     },
     about : (req, res) => {
         res.render('home/about');
