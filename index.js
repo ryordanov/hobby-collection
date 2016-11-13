bfdfc6937876fb3d4a7b77d8f54e13ec31b5d4a7
ryordanov <rosen.gerov@gmail.com>
2016-11-13 21:12:50 +0200

    Merge remote-tracking branch 'origin/master'

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ let app = express();
 
 let config = require('./server/config/config');
 
-require('./server/config/database')(config);
+//require('./server/config/database')(config);
 require('./server/config/express')(config, app);
 require('./server/config/routes')(app);
 
 app.listen(config.port);
-console.log('Express is ready!');
+//console.log('Express is ready!');
