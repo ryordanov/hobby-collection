43020d519d7999b8f68c66df127fc4265d02fd5d
ryordanov <rosen.gerov@gmail.com>
2016-11-18 11:35:46 +0200

    Update config.js
    heroku problems

--- server/config/config.js
+++ server/config/config.js
@@ -2,18 +2,18 @@
 const path = require('path');
 let rootPath = path.normalize(path.join(__dirname, '../../'));
 var hostname = require("os").hostname() || 'localhost';
-
+/*
 if (doesExist('../../../private-configuration.js')) {
     var privateConfiguration = require('../../../private-configuration.js')[hostname];
 }
-
+*/
 console.log('hostname: ' + hostname);
 console.log('------------------------------');
 
 module.exports = {
     rootPath: rootPath,
-    db: privateConfiguration.db || 'mongodb://localhost:27017/guminserts-db',
-    port: process.env.PORT || privateConfiguration.port || 10101
+    db: process.env.DB || /*privateConfiguration.db ||*/ 'mongodb://localhost:27017/guminserts-db',
+    port: process.env.PORT ||/* privateConfiguration.port ||*/ 10101
 };
 
 function doesExist(fileName) {
