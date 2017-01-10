19b39ab179f0999844dc07bd9a1f3ab1265cece5
Rosen Yordanov <workbox@abv.bg>
2017-01-10 20:57:11 +0200

    fix: eslint config

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ require('./server/config/express')(config, app);
 require('./server/config/routes')(app);
 
 app.listen(config.port, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
