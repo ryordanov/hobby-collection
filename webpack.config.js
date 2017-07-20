7a8fca125277fe7dc9dceb06aafae470f85f0b09
Rosen Yordanov <rosen.yordanov@Softwaregroup-bg.com>
2017-07-20 17:13:21 +0300

    fix: webpack configuration - adding cache

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
+                loader: 'babel-loader?cacheDirectory=true',
                 options: {
                     presets: ['es2015', 'react']
                 }
